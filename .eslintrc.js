bd66c5d8f005a839f262547152336b3081f6f83c
Galen Corey <galen.corey@gmail.com>
2018-02-21 17:03:29 -0500

    es6 and comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  // extends: '@simple-contacts',
+  extends: '@simple-contacts',
 };
---
+++ s3util.js
@@ -0,0 +1,30 @@
+import AWS from 'aws-sdk';
+import conf from './config';
+import shortid from 'shortid';
+import moment from 'moment';
+
+async function uploadS3(video, key) {
+  const params = {
+    Bucket: conf.get('aws_s3_exam_bucket'),
+    Key: key,
+    Body: video,
+  };
+
+  AWS.config.update({
+    accessKeyId: conf.get('aws_access_key'),
+    secretAccessKey: conf.get('aws_secret_key'),
+  });
+
+  const s3 = new AWS.S3();
+  const asyncUpload = s3.upload(params).promise();
+  await asyncUpload;
+}
+
+function createS3Key(fileExtension) {
+  const date = moment().format('YYYY-MM-DD');
+  const epoch = +new Date();
+  const rando = shortid.generate();
+
+  return `${date}/kurento${epoch}-${rando}.${fileExtension}`;
+}
+export { uploadS3, createS3Key };
