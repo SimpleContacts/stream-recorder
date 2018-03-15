d5a3986e624e2d254bc2aefd5dc70b3ab9969d1e
Steve Farthing <me@stevefar.com>
2018-03-15 14:40:38 -0400

    Fix errors

--- client/getDebuggingInfo.js
+++ client/getDebuggingInfo.js
@@ -1,35 +1,43 @@
 export default video => {
-  let videoBytesSent = 0;
-  const candidatesPairs = [];
+  try {
+    let videoBytesSent = 0;
+    const candidatesPairs = [];
 
-  video.forEach(s => {
-    if (s.type === 'candidate-pair') {
-      candidatesPairs.push(s);
-    }
-    if (s.type === 'outbound-rtp') {
-      videoBytesSent += s.bytesSent;
-    }
-  });
+    video.forEach(s => {
+      if (s.type === 'candidate-pair') {
+        candidatesPairs.push(s);
+      }
+      if (s.type === 'outbound-rtp') {
+        videoBytesSent += s.bytesSent;
+      }
+    });
 
-  const candidatePair = candidatesPairs.reduce((acc, c) => {
-    if (c.bytesSent >= acc.bytesSent) {
-      return c;
-    }
-    return acc;
-  });
+    const candidatePair = candidatesPairs.reduce((acc, c) => {
+      if (c.bytesSent >= acc.bytesSent) {
+        return c;
+      }
+      return acc;
+    });
 
-  const remoteCandidate = video.get(candidatePair.remoteCandidateId);
-  const localCandidate = video.get(candidatePair.localCandidateId);
+    const remoteCandidate = video.get(candidatePair.remoteCandidateId);
+    const localCandidate = video.get(candidatePair.localCandidateId);
 
-  const destination =
-    remoteCandidate &&
-    `${remoteCandidate.ip}:${remoteCandidate.port} ${remoteCandidate.protocol}`;
+    const destination =
+      remoteCandidate &&
+      `${remoteCandidate.ip}:${remoteCandidate.port} ${
+        remoteCandidate.protocol
+      }`;
 
-  return {
-    videoBytesSent,
-    remoteCandidate,
-    localCandidate,
-    networkType: localCandidate && localCandidate.networkType,
-    destination,
-  };
+    return {
+      videoBytesSent,
+      remoteCandidate,
+      localCandidate,
+      networkType: localCandidate && localCandidate.networkType,
+      destination,
+    };
+  } catch (e) {
+    console.error(e);
+  }
+
+  return {};
 };
