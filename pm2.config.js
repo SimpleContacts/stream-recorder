ebf7bc29d41d1297f02bc86157a2b98d191a8c89
Steve Farthing <steve@simplecontacts.com>
2019-08-13 15:57:55 -0400

    Make mobile friendly by allowing the dev server to run on a host thats not localhost and fix constraints for mobile browsers

--- pm2.config.js
+++ pm2.config.js
@@ -14,6 +14,7 @@ module.exports = {
             args: `
               client/admin.js
               --mode development
+              --host 0.0.0.0
               --port 8080
               --info false
               --public localhost:8088
